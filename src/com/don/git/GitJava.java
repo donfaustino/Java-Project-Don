da4704bcf877ec3501d7df8ce242d7fe56f7e2c1
Mark Louie Baquit <mark.baquit@orangeandbronze.com>
2013-06-05 13:47:57 +0800

    added new function

--- src/com/don/git/GitJava.java
+++ src/com/don/git/GitJava.java
@@ -3,5 +3,11 @@ package com.don.git;
 public class GitJava {
 	public String printHelloWorld() {
 		return "Hello World edited test";
+		
 	}
+	public String printHelloWorld1() {
+		return "Hello World edited test";
+		
+	}
+	
 }
